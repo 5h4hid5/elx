e03441cf9a3e15262d5eba6ceccd03beca554f86
5h4hid5 <shahid.bracu@gmail.com>
2017-05-19 19:22:51 +0600

    Completed base site

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-$(document).ready(function(){
-    $(window).bind('scroll', function() {
-    var navHeight = $( window ).height() - $('nav').height();
-      if ($(window).scrollTop() > navHeight) {
-        $('nav').addClass('navbar-fixed-top');
-      }
-      else {
-        $('nav').removeClass('navbar-fixed-top');
-      }
-   });
-
-
-   Map();
-
- });
+$(document).ready(function() {
+  $(window).bind('scroll', function() {
+    var navHeight = $(window).height() - $('nav').height();
+    if ($(window).scrollTop() > navHeight) {
+      $('nav').addClass('navbar-fixed-top');
+    } else {
+      $('nav').removeClass('navbar-fixed-top');
+    }
+  });
+  Map();
+  $("#open-form").click(function(e) {
+    e.preventDefault();
+    console.log("Hello");
+    $("#form-container").css("display","flex");
+  });
+  $(".close-btn").click(function(e) {
+    e.preventDefault();
+    $("#form-container").css("display","none");
+  });
+});
--- js/contact.js
+++ js/contact.js
@@ -1,73 +1,63 @@
-var Contact = function () {
 
-    var init = function () {
 
-        // body clicks
-        $(document).mouseup(function (e) {
-            if (!$('.contact').is(e.target) && $('.contact').has(e.target).length === 0) {
-                $('#content').removeClass('contact-open');
-                $('.main-nav').removeClass('main-nav-open');
-            }
-        });
+var init = function () {
+    // body clicks
+    $(document).mouseup(function (e) {
+        if (!$('.contact').is(e.target) && $('.contact').has(e.target).length === 0) {
+            $('#content').removeClass('contact-open');
+            $('.main-nav').removeClass('main-nav-open');
+        }
+    });
+};
 
-    };
+// toggle contact
+var showContact = function () {
+    $('#content').toggleClass('contact-open');
+    $('.main-nav').toggleClass('main-nav-open');
+};
 
-    // toggle contact
-    var showContact = function () {
-        $('#content').toggleClass('contact-open');
-        $('.main-nav').toggleClass('main-nav-open');
-    };
+// check required fields
+var checkForm = function (id) {
 
-    // check required fields
-    var checkForm = function (id) {
+    var valid = true;
 
-        var valid = true;
+    $(id).find('.req').each(function (index) {
 
-        $(id).find('.req').each(function (index) {
+        if ($(this).val() == $(this).prop('defaultValue') || $(this).val() == '') {
 
-            if ($(this).val() == $(this).prop('defaultValue') || $(this).val() == '') {
+            $(this).addClass('errortxt');
+            valid = false;
 
-                $(this).addClass('errortxt');
-                valid = false;
+            $('#contact-form-error').html('* Missing required fields');
 
-                $('#contact-form-error').html('* Missing required fields');
+        } else {
 
-            } else {
+            $(this).removeClass('errortxt');
 
-                $(this).removeClass('errortxt');
+        }
 
-            }
+    });
 
-        });
+    if (valid) sendForm(id);
 
-        if (valid) sendForm(id);
+    return false;
 
-        return false;
+};
 
-    };
+var sendForm = function (id) {
 
-    var sendForm = function (id) {
+    var formData = $('#form-contact').serialize();
 
-        var formData = $('#form-contact').serialize();
-
-        $.ajax({
-            type: 'POST',
-            url: 'http://mail.elixeur.com/send.php',
-            data: formData
-        }).success(function () {
-
-            $('#contact-content').html('<b>Thank you for contacting us! We will get back to you as soon as possible!</b><br/><br/><p><a href="javascript:Contact.showContact();" class="formsbtn">Close</a></p>');
-
-        });
+    $.ajax({
+        type: 'POST',
+        url: 'http://mail.elixeur.com/send.php',
+        data: formData
+    }).success(function () {
 
         $('#contact-content').html('<b>Thank you for contacting us! We will get back to you as soon as possible!</b><br/><br/><p><a href="javascript:Contact.showContact();" class="formsbtn">Close</a></p>');
 
-    };
+    });
 
-    return {
-        init: init,
-        showContact: showContact,
-        checkForm: checkForm
-    };
-    init();
+    $('#contact-content').html('<b>Thank you for contacting us! We will get back to you as soon as possible!</b><br/><br/><p><a href="javascript:Contact.showContact();" class="formsbtn">Close</a></p>');
+
 };
