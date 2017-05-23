87dd17e0fdc7e3db0defacb8399f1d5bb45b09f1
5h4hid5 <shahid.bracu@gmail.com>
2017-05-23 22:02:37 +0600

    Ne

--- js/app.js
+++ js/app.js
@@ -1,20 +1,125 @@
 $(document).ready(function() {
-  $(window).bind('scroll', function() {
-    var navHeight = $(window).height() - $('nav').height();
-    if ($(window).scrollTop() > navHeight) {
-      $('nav').addClass('navbar-fixed-top');
-    } else {
-      $('nav').removeClass('navbar-fixed-top');
-    }
+  var height = $(window).height();
+  $('nav').attr("data-offset-top", height);
+  $(window).scroll(function() {
+    var height = $(window).height();
+    $('nav').attr("data-offset-top", height);
   });
   Map();
-  $("#open-form").click(function(e) {
-    e.preventDefault();
-    console.log("Hello");
-    $("#form-container").css("display","flex");
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
-  $(".close-btn").click(function(e) {
-    e.preventDefault();
-    $("#form-container").css("display","none");
+
+  let stagesDes = [
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda possimus, quas, provident debitis excepturi.",
+    "Ii dolor sit amet, consectetur adipisicing elit. Assumenda possimus, quas, provident debitis excepturi",
+    "Sit amet, consectetur adipisicing elit. Assumenda possimus, quas, provident debitis excepturi",
+    "Lello Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda possimus, quas, provident debitis excepturi"
+  ];
+
+  $(".search").hover(
+    function() {
+      $(".ser").css("opacity", 1);
+    },
+    function() {
+      $(".ser").css("opacity", 0);
+    }
+  );
+  $(".execute").hover(
+    function() {
+      $(".exc").css("opacity", 1);
+    },
+    function() {
+      $(".exc").css("opacity", 0);
+    }
+  );
+
+  $(".cd").hover(function() {
+    $("#stg-des").removeClass("text-right").addClass("text-left").text(stagesDes[0]);
+  }, function() {
+    $("#stg-des").text("");
   });
+  $(".cv").hover(function() {
+    $("#stg-des").removeClass("text-right").addClass("text-left").text(stagesDes[1]);
+  }, function() {
+    $("#stg-des").text("");
+  });
+  $(".cc").hover(function() {
+    $("#stg-des").removeClass("text-left").addClass("text-right").text(stagesDes[2]);
+  }, function() {
+    $("#stg-des").text("");
+  });
+  $(".cb").hover(function() {
+    $("#stg-des").removeClass("text-left").addClass("text-right").text(stagesDes[0]);
+  }, function() {
+    $("#stg-des").text("");
+  });
+
+  $('.psf').hover(
+    function(){
+      $('#psf').removeClass("fill-white").addClass("fill-red");
+      $('#ladder1').removeClass("fill-white").addClass("fill-red");
+    },
+    function(){
+      $('#psf').removeClass("fill-red").addClass("fill-white");
+      $('#ladder1').removeClass("fill-red").addClass("fill-white");
+    }
+  );
+  $('.pmf').hover(
+    function(){
+      $('#pmf').removeClass("fill-white").addClass("fill-red");
+      $('#ladder2').removeClass("fill-white").addClass("fill-red");
+    },
+    function(){
+      $('#pmf').removeClass("fill-red").addClass("fill-white");
+      $('#ladder2').removeClass("fill-red").addClass("fill-white");
+    }
+  );
+  $('.bmf').hover(
+    function(){
+      $('#bmf').removeClass("fill-white").addClass("fill-red");
+      $('#ladder3').removeClass("fill-white").addClass("fill-red");
+    },
+    function(){
+      $('#bmf').removeClass("fill-red").addClass("fill-white");
+      $('#ladder3').removeClass("fill-red").addClass("fill-white");
+    }
+  );
+
+  var Elixeur = (function ($) {
+
+      var init = function () {
+            // contact functionality
+          if (typeof Contact !== 'undefined')
+              Contact.init();
+      };
+      return {
+          init: init
+      };
+
+  }(jQuery));
+
+  $(function () {
+      Elixeur.init();
+  });
+
 });
--- js/contact.js
+++ js/contact.js
@@ -1,63 +1,71 @@
+var Contact = (function ($) {
 
+    var init = function () {
 
-var init = function () {
-    // body clicks
-    $(document).mouseup(function (e) {
-        if (!$('.contact').is(e.target) && $('.contact').has(e.target).length === 0) {
-            $('#content').removeClass('contact-open');
-            $('.main-nav').removeClass('main-nav-open');
-        }
-    });
-};
+        // body clicks
+        $(document).mouseup(function (e) {
+            if (!$('.contact').is(e.target) && $('.contact').has(e.target).length === 0) {
+                $('#content').removeClass('contact-open');
+            }
+        });
 
-// toggle contact
-var showContact = function () {
-    $('#content').toggleClass('contact-open');
-    $('.main-nav').toggleClass('main-nav-open');
-};
+    };
 
-// check required fields
-var checkForm = function (id) {
+    // toggle contact
+    var showContact = function () {
+        $('#content').toggleClass('contact-open');
+    };
 
-    var valid = true;
+    // check required fields
+    var checkForm = function (id) {
 
-    $(id).find('.req').each(function (index) {
+        var valid = true;
 
-        if ($(this).val() == $(this).prop('defaultValue') || $(this).val() == '') {
+        $(id).find('.req').each(function (index) {
 
-            $(this).addClass('errortxt');
-            valid = false;
+            if ($(this).val() == $(this).prop('defaultValue') || $(this).val() == '') {
 
-            $('#contact-form-error').html('* Missing required fields');
+                $(this).addClass('errortxt');
+                valid = false;
 
-        } else {
+                $('#contact-form-error').html('* Missing required fields');
 
-            $(this).removeClass('errortxt');
+            } else {
 
-        }
+                $(this).removeClass('errortxt');
 
-    });
+            }
 
-    if (valid) sendForm(id);
+        });
 
-    return false;
+        if (valid) sendForm(id);
 
-};
+        return false;
 
-var sendForm = function (id) {
+    };
 
-    var formData = $('#form-contact').serialize();
+    var sendForm = function (id) {
 
-    $.ajax({
-        type: 'POST',
-        url: 'http://mail.elixeur.com/send.php',
-        data: formData
-    }).success(function () {
+        var formData = $('#form-contact').serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://mail.elixeur.com/send.php',
+            data: formData
+        }).success(function () {
+
+            $('#contact-content').html('<b>Thank you for contacting us! We will get back to you as soon as possible!</b><br/><br/><p><a href="javascript:Contact.showContact();" class="formsbtn">Close</a></p>');
+
+        });
 
         $('#contact-content').html('<b>Thank you for contacting us! We will get back to you as soon as possible!</b><br/><br/><p><a href="javascript:Contact.showContact();" class="formsbtn">Close</a></p>');
 
-    });
+    };
 
-    $('#contact-content').html('<b>Thank you for contacting us! We will get back to you as soon as possible!</b><br/><br/><p><a href="javascript:Contact.showContact();" class="formsbtn">Close</a></p>');
+    return {
+        init: init,
+        showContact: showContact,
+        checkForm: checkForm
+    };
 
-};
+}(jQuery));
